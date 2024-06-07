0cde6a070cef2986b4415e8798acf48360ea5956
61rita <163466652+61rita@users.noreply.github.com>
2024-06-07 16:15:58 +0100

    Javascript - Main Navigation, Contact Form

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,11 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('expanded');
-  });
+  document.querySelector('.nav-links').classList.toggle('expanded');
+});
+
+// Active links and pages styles
+var anchor = document.querySelector('.nav-links').querySelectorAll('a');
+for (var i = 0; i < anchor.length; i++) {
+  if (window.location.href === anchor[i].href) {
+    anchor[i].classList.add("active");
+  }
+}
